fe4b3f01ee787c845b9af4c4fbfa2eab95fdbec1
acetilsalicilic <1Uncanalmas@gmail.com>
2025-07-31 14:20:23 -0600

    Some testing for the family tree

--- test/proc/test_gen_tree.py
+++ test/proc/test_gen_tree.py
@@ -1,16 +1,74 @@
-from proc.npc.GenTree import GenTree
-from proc.npc.NPC import NPC
-
 import unittest
 
+from proc.npc.GenTree import GenTree
+from proc.npc.GenTree import GenTreeIntegrityError
+from proc.npc.NPC import NPC
+
 class TestGenTree(unittest.TestCase):
-    def test_init_tree_gen(self):
-        n1 = NPC('1', 'female', 'Eva')
-        n2 = NPC('2', 'male', 'Adan')
+    def test_tree_creation(self):
+        print('---CREATION---')
+        adan = NPC()
+        eva = NPC()
+        gen_tree = GenTree(adan, eva)
 
-        gt = GenTree(n1, n2)
-        dm = gt.get_dist_mat()
-        dm.print_matrix()
+        gen_tree.print_mesh()
+    
+    def test_first_child(self):
+        print('---FIRST CHILD---')
+        adan = NPC('Adan')
+        eva = NPC('Eva')
+        child = NPC('Cain')
 
-if __name__ == '__main__':
-    unittest.main()
+        gen_tree = GenTree(adan, eva)
+
+        gen_tree.add_npc(child, adan, eva)
+
+        gen_tree.print_mesh()
+    
+    def test_second_child(self):
+        print('---SECOND CHILD---')
+        adan = NPC('Adan')
+        eva = NPC('Eva')
+        cain = NPC('Cain')
+        abel = NPC('Abel')
+
+        gen_tree = GenTree(adan, eva)
+
+        gen_tree.add_npc(cain, adan, eva)
+        gen_tree.add_npc(abel, adan, eva)
+
+        gen_tree.print_mesh()
+    
+    def test_no_intergeneration(self):
+        print('---INCEST---')
+        adan = NPC('Adan')
+        eva = NPC('Eva')
+        cain = NPC('Cain')
+        child = NPC('Bad one')
+
+        gen_tree = GenTree(adan, eva)
+
+        gen_tree.add_npc(cain, adan, eva)
+        self.assertRaises(GenTreeIntegrityError, gen_tree.add_npc, child, cain, eva)
+
+        gen_tree.print_mesh()
+    
+    def test_non_existent(self):
+        print('---NON EXISTENT---')
+        first = NPC('First')
+        second = NPC('SECOND')
+        child = NPC('child')
+        nobody = NPC()
+
+        gen_tree = GenTree(first, second)
+        self.assertRaises(GenTreeIntegrityError, gen_tree.add_npc, child, first, nobody)
+    
+    def test_insert_again(self):
+        adan = NPC('Adan')
+        eva = NPC('Eva')
+        child = NPC('Cain')
+
+        gen_tree = GenTree(adan, eva)
+
+        gen_tree.add_npc(child, adan, eva)
+        self.assertRaises(GenTreeIntegrityError, gen_tree.add_npc, adan, child, eva)
