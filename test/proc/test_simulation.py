f3c7697ced990ba8f06e123ef844861383f01e4b
acetilsalicilic <1Uncanalmas@gmail.com>
2025-08-02 21:21:01 -0600

    Updated simulation testing test case

--- test/proc/test_simulation.py
+++ test/proc/test_simulation.py
@@ -1,9 +1,19 @@
+import os
 import unittest
 
+import logging
+from sim_logging.ProcLogger import ProgressLogger, SimReportLogger
 from proc.main_simulation import ProceduralSimulation
 
 class TestSimulation(unittest.TestCase):
     def test_ten_steps(self):
-        simulation = ProceduralSimulation(1)
-        simulation.simulate(10)
+        # loggers
+        log_file = os.getcwd() + '/results.log'
+        mod_name = __name__
+
+        progress = ProgressLogger(log_file, mod_name)
+        report = SimReportLogger(log_file, mod_name)
+
+        simulation = ProceduralSimulation(1, proc_logger=progress, report_logger=report)
+        simulation.simulate(3)
         simulation.report_simulation()
